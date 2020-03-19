babb365840b182844d7e5385c302e219515784d0
Anar Ismayilov <anar_ismayilov@ymail.com>
2020-03-19 09:08:17 +0400

    final

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ function showResult() {
     let term = + document.getElementById("term").value;
     let totalSum = sum;
     let monthInterest = interest / 100 / 12;
-    let monthes = Math.ceil(term / 30);
+    let monthes = Math.floor(term / 30);
     let table = document.getElementById("myTable");
     clearTable(table);
-    
+
 
     if (check(sum, add, interest, term) === false) {
         document.getElementById("false-result").style.display = "block";
@@ -20,13 +20,13 @@ function showResult() {
     } else {
         if (addPeriod == 2) {
             for (let i = 1; i <= monthes; i++) {
+                let row = table.insertRow(-1);
+                let cell1 = row.insertCell(0);
+                let cell2 = row.insertCell(1);
+                let cell3 = row.insertCell(2);
+                let cell4 = row.insertCell(3);
+                let cell5 = row.insertCell(4);
                 if (i == 1) {
-                    let row = table.insertRow(-1);
-                    let cell1 = row.insertCell(0);
-                    let cell2 = row.insertCell(1);
-                    let cell3 = row.insertCell(2);
-                    let cell4 = row.insertCell(3);
-                    let cell5 = row.insertCell(4);
                     cell2.innerHTML = totalSum;
                     cell3.innerHTML = Math.round((totalSum * (1 + monthInterest) - totalSum) * 100) / 100;
                     totalSum = Math.round(totalSum * (1 + monthInterest) * 100) / 100;
@@ -34,12 +34,6 @@ function showResult() {
                     cell4.innerHTML = 0;
                     cell5.innerHTML = totalSum;
                 } else if (i % 4 == 0) {
-                    let row = table.insertRow(-1);
-                    let cell1 = row.insertCell(0);
-                    let cell2 = row.insertCell(1);
-                    let cell3 = row.insertCell(2);
-                    let cell4 = row.insertCell(3);
-                    let cell5 = row.insertCell(4);
                     cell2.innerHTML = totalSum;
                     cell3.innerHTML = Math.round((totalSum * (1 + monthInterest) - totalSum) * 100) / 100;
                     totalSum = Math.round((totalSum + add) * (1 + monthInterest) * 100) / 100;
@@ -47,12 +41,6 @@ function showResult() {
                     cell4.innerHTML = add;
                     cell5.innerHTML = totalSum;
                 } else {
-                    let row = table.insertRow(-1);
-                    let cell1 = row.insertCell(0);
-                    let cell2 = row.insertCell(1);
-                    let cell3 = row.insertCell(2);
-                    let cell4 = row.insertCell(3);
-                    let cell5 = row.insertCell(4);
                     cell2.innerHTML = totalSum;
                     cell3.innerHTML = Math.round((totalSum * (1 + monthInterest) - totalSum) * 100) / 100;
                     totalSum = Math.round((totalSum) * (1 + monthInterest) * 100) / 100;
@@ -64,13 +52,13 @@ function showResult() {
 
         } else if (addPeriod == 3) {
             for (let i = 1; i <= monthes; i++) {
+                let row = table.insertRow(-1);
+                let cell1 = row.insertCell(0);
+                let cell2 = row.insertCell(1);
+                let cell3 = row.insertCell(2);
+                let cell4 = row.insertCell(3);
+                let cell5 = row.insertCell(4);
                 if (i == 1) {
-                    let row = table.insertRow(-1);
-                    let cell1 = row.insertCell(0);
-                    let cell2 = row.insertCell(1);
-                    let cell3 = row.insertCell(2);
-                    let cell4 = row.insertCell(3);
-                    let cell5 = row.insertCell(4);
                     cell2.innerHTML = totalSum;
                     cell3.innerHTML = Math.round((totalSum * (1 + monthInterest) - totalSum) * 100) / 100;
                     totalSum = Math.round(totalSum * (1 + monthInterest) * 100) / 100;
@@ -78,12 +66,6 @@ function showResult() {
                     cell4.innerHTML = 0;
                     cell5.innerHTML = totalSum;
                 } else if (i % 12 == 0) {
-                    let row = table.insertRow(-1);
-                    let cell1 = row.insertCell(0);
-                    let cell2 = row.insertCell(1);
-                    let cell3 = row.insertCell(2);
-                    let cell4 = row.insertCell(3);
-                    let cell5 = row.insertCell(4);
                     cell2.innerHTML = totalSum;
                     cell3.innerHTML = Math.round((totalSum * (1 + monthInterest) - totalSum) * 100) / 100;
                     totalSum = Math.round((totalSum + add) * (1 + monthInterest) * 100) / 100;
@@ -91,12 +73,6 @@ function showResult() {
                     cell4.innerHTML = add;
                     cell5.innerHTML = totalSum;
                 } else {
-                    let row = table.insertRow(-1);
-                    let cell1 = row.insertCell(0);
-                    let cell2 = row.insertCell(1);
-                    let cell3 = row.insertCell(2);
-                    let cell4 = row.insertCell(3);
-                    let cell5 = row.insertCell(4);
                     cell2.innerHTML = totalSum;
                     cell3.innerHTML = Math.round((totalSum * (1 + monthInterest) - totalSum) * 100) / 100;
                     totalSum = Math.round((totalSum) * (1 + monthInterest) * 100) / 100;
@@ -108,13 +84,14 @@ function showResult() {
 
         } else {
             for (let i = 1; i <= monthes; i++) {
+                let row = table.insertRow(-1);
+                let cell1 = row.insertCell(0);
+                let cell2 = row.insertCell(1);
+                let cell3 = row.insertCell(2);
+                let cell4 = row.insertCell(3);
+                let cell5 = row.insertCell(4);
                 if (i == 1) {
-                    let row = table.insertRow(-1);
-                    let cell1 = row.insertCell(0);
-                    let cell2 = row.insertCell(1);
-                    let cell3 = row.insertCell(2);
-                    let cell4 = row.insertCell(3);
-                    let cell5 = row.insertCell(4);
+
                     cell2.innerHTML = totalSum;
                     cell3.innerHTML = Math.round((totalSum * (1 + monthInterest) - totalSum) * 100) / 100;
                     totalSum = Math.round(totalSum * (1 + monthInterest) * 100) / 100;
@@ -122,12 +99,6 @@ function showResult() {
                     cell4.innerHTML = 0;
                     cell5.innerHTML = totalSum;
                 } else {
-                    let row = table.insertRow(-1);
-                    let cell1 = row.insertCell(0);
-                    let cell2 = row.insertCell(1);
-                    let cell3 = row.insertCell(2);
-                    let cell4 = row.insertCell(3);
-                    let cell5 = row.insertCell(4);
                     cell2.innerHTML = totalSum;
                     cell3.innerHTML = Math.round((totalSum * (1 + monthInterest) - totalSum) * 100) / 100;
                     totalSum = Math.round((totalSum + add) * (1 + monthInterest) * 100) / 100;
@@ -144,24 +115,24 @@ function showResult() {
 }
 
 function check(sum, add, interest, term) {
-    if (sum <= 0 ||isNaN(sum)) {
-            document.getElementById("false-result").innerHTML = "Ошибка при вводе начального вклада: "+ 
+    if (sum <= 0 || isNaN(sum)) {
+        document.getElementById("false-result").innerHTML = "Ошибка при вводе начального вклада: " +
             "сумма должна быть больше нуля и написана цифрами";
-            return false;
-        }
-    else if  (add < 0 || isNaN(add)){
-        document.getElementById("false-result").innerHTML = "Ошибка при вводе пополнений: "+ 
-        "пополнения не могут быть меньше нуля и размер пополнений должен быть написан цифрами";
         return false;
     }
-    else if  (interest <= 0 || interest > 100 || isNaN(interest)){
-        document.getElementById("false-result").innerHTML = "Ошибка при вводе процентной ставки: "+ 
-        "процентная ставка должна быть выше нуля и размер ставки должен быть написан цифрами";
+    else if (add < 0 || isNaN(add)) {
+        document.getElementById("false-result").innerHTML = "Ошибка при вводе пополнений: " +
+            "пополнения не могут быть меньше нуля и размер пополнений должен быть написан цифрами";
         return false;
     }
-    else if ((term <= 0 || term % 1 != 0)||isNaN(term)) {
-        document.getElementById("false-result").innerHTML = "Ошибка при вводе срока: "+ 
-        "срок должен быть выше нуля и срок должен быть написан цифрами";
+    else if (interest <= 0 || interest > 100 || isNaN(interest)) {
+        document.getElementById("false-result").innerHTML = "Ошибка при вводе процентной ставки: " +
+            "процентная ставка должна быть выше нуля и размер ставки должен быть написан цифрами";
+        return false;
+    }
+    else if ((term <= 0 || term % 1 != 0) || isNaN(term)) {
+        document.getElementById("false-result").innerHTML = "Ошибка при вводе срока: " +
+            "срок должен быть выше нуля и срок должен быть написан цифрами";
         return false;
     } else return true;
 }
